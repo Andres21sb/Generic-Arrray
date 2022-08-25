8a79f93e478f811aa41ca7cdc4f9266ee12c8f5e
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2022-08-25 15:27:52 -0600

    .
    .

---
+++ cmake-build-debug/CMakeFiles/GenericArray.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Andres Mendez Solano/CLionProjects/Generic array/Generic-Arrray/coordenada.cpp" "CMakeFiles/GenericArray.dir/coordenada.cpp.obj" "gcc" "CMakeFiles/GenericArray.dir/coordenada.cpp.obj.d"
+  "C:/Users/Andres Mendez Solano/CLionProjects/Generic array/Generic-Arrray/genericArray.cpp" "CMakeFiles/GenericArray.dir/genericArray.cpp.obj" "gcc" "CMakeFiles/GenericArray.dir/genericArray.cpp.obj.d"
+  "C:/Users/Andres Mendez Solano/CLionProjects/Generic array/Generic-Arrray/main.cpp" "CMakeFiles/GenericArray.dir/main.cpp.obj" "gcc" "CMakeFiles/GenericArray.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/GenericArray.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenericArray.dir/coordenada.cpp.obj"
+  "CMakeFiles/GenericArray.dir/coordenada.cpp.obj.d"
+  "CMakeFiles/GenericArray.dir/genericArray.cpp.obj"
+  "CMakeFiles/GenericArray.dir/genericArray.cpp.obj.d"
+  "CMakeFiles/GenericArray.dir/main.cpp.obj"
+  "CMakeFiles/GenericArray.dir/main.cpp.obj.d"
+  "GenericArray.exe"
+  "GenericArray.exe.manifest"
+  "GenericArray.pdb"
+  "libGenericArray.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenericArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
